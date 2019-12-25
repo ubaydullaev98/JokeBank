c246f0660a7539a009c156709292b3241dfa4fc0
Dilmurod Ubaydullaev <dilmurodubaydullaev22@gmail.com>
2019-12-25 14:58:04 +0500

    Add jokes

--- JokeBank/JokeDefinitionViewController.swift
+++ JokeBank/JokeDefinitionViewController.swift
@@ -10,13 +10,45 @@ import UIKit
 
 class JokeDefinitionViewController: UIViewController {
     
+    @IBOutlet weak var JokeDefinitionLabel: UILabel!
+    
+    @IBOutlet weak var JokeAnswerLabel: UILabel!
+    
     var joke = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         title = joke
+        JokeAnswerLabel.text = ""
+        
+        if(joke=="Chicken"){
+            JokeDefinitionLabel.text = "Q: Why did the chicken cross the road?"
+        }
+        if(joke=="Walk into a bar..."){
+            JokeDefinitionLabel.text = "Q: A horse walks into a bar and the bartender says 'Why the long face?'"
+        }
+        if(joke=="Racer"){
+            JokeDefinitionLabel.text = "Q: What did the hot dog say when he crossed the finish line?"
+        }
+        if(joke=="Olive"){
+            JokeDefinitionLabel.text = "Q: Did not create yet. Sorry!"
+        }
     }
     
-
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        if(joke=="Chicken"){
+            JokeAnswerLabel.text = "A: To get to the other side."
+        }
+        if(joke=="Walk into a bar..."){
+            JokeAnswerLabel.text = "A: The bartender is drunk."
+        }
+        if(joke=="Racer"){
+            JokeAnswerLabel.text = "A: I am the weiner."
+        }
+        if(joke=="Olive"){
+            JokeAnswerLabel.text = "A: Did not create yet. Sorry!"
+        }
+    }
+    
 }
